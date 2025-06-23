23db58e69d298b883c7dfd14f437e3656a92fd59
ThanhTumg <thanhtumg.2510@gmail.com>
2025-06-23 22:17:20 +0700

    Feature: Add Swagger UI and OpenAPI docs for API

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 require("express-async-errors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./openapi.json");
 
 // extra security
 const helmet = require("helmet");
@@ -33,6 +35,14 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+// Serve Swagger UI at /api-docs
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Example route
+app.get("/", (req, res) => {
+  res.send("Welcome to JOBS API. Visit /api-docs for documentation.");
+});
 
 // routes
 app.use("/api/v1/auth", authRouter);
